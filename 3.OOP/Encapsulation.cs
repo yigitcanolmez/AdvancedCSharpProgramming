eba0b9be1e421259f9705aef491b83c654c8f320
Yiğitcan Ölmez <yigitcan.olmez@d-teknoloji.com.tr>
2023-09-14 08:47:11 +0300

    encapsulation

---
+++ 3.OOP/Encapsulation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.OOP
+{
+    public class Encapsulation
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Identity { get; set; }
+        private double Salary { get; set; }
+
+        public void SetPersonSalary(double value)
+        {
+            if (value > 11000)
+                Salary = value;
+            else
+                throw new Exception("Salary can not be lower than 11.000!");
+        }
+
+        public double GetSalary() { return Salary; }
+
+    }
+}
