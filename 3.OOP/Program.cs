f1123e09be26062c533283f91edfe7281b6b9249
Yiğitcan Ölmez <yigitcan.olmez@d-teknoloji.com.tr>
2023-09-14 13:40:39 +0300

    .

--- 3.OOP/Program.cs
+++ 3.OOP/Program.cs
@@ -1,19 +1,32 @@
 ﻿using _3.OOP;
-using System;
 
-Console.WriteLine("dsadasd :");
-int a = Convert.ToInt16(Console.ReadLine());
+//Console.WriteLine("dsadasd :");
+//int a = Convert.ToInt16(Console.ReadLine());
 
 //var a = (double)241 / (300 * 0.01);
 //Console.WriteLine(string.Format("{0:0.0000}", a)
 //);
-Console.WriteLine("sssssssssssssssss");
+//Console.WriteLine("sssssssssssssssss");
 
-Encapsulation encapsulation = new Encapsulation() { Id = Guid.NewGuid(), Identity = "12312312312", Name = "Yılmaz" };
+//Encapsulation encapsulation = new Encapsulation() { Id = Guid.NewGuid(), Identity = "12312312312", Name = "Yılmaz" };
 
-encapsulation.SetPersonSalary(12000);
-var salary = encapsulation.GetSalary();
+//encapsulation.SetPersonSalary(12000);
+//var salary = encapsulation.GetSalary();
 
-Console.WriteLine(salary);
+
+
+List<Language> sayHello = new List<Language>();
+
+sayHello.Add(new German());
+sayHello.Add(new Turkish());
+sayHello.Add(new Italian());
+
+foreach (var item in sayHello)
+{
+    item.Speak();
+}
+
+
+//Console.WriteLine(salary);
 
 Console.ReadKey();
