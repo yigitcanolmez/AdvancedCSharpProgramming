7b974c9acadd4ad371b02a09688bc7676ce1ff42
Yiğitcan Ölmez <yigitcan.olmez@d-teknoloji.com.tr>
2023-09-16 19:30:25 +0300

    learnt factoryMethod

--- 4.Design Patterns/Creational Design Pattern/FactoryMethodDesignPattern.cs
+++ 4.Design Patterns/Creational Design Pattern/FactoryMethodDesignPattern.cs
@@ -1,8 +1,67 @@
-﻿ 
+﻿
+
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Channels;
 
 namespace _4.Design_Patterns.Creational_Design_Pattern
 {
     public class FactoryMethodDesignPattern
     {
+        //public class IslakHamburger
+        //{
+        //    public List<string> Malzemeler { get; set; }
+        //    public double Fiyat { get; set; }
+        //    public string ÜrünAdı { get; set; }
+        //}
+        //public void Siparis(IslakHamburger istek)
+        //{
+        //    //ürün adı boş ise throw atılacaktır.
+        //    ArgumentNullException.ThrowIfNullOrEmpty(istek.ÜrünAdı);
+
+        //    Console.WriteLine($"{istek.ÜrünAdı} siparişiniz {istek.Fiyat} TL tutarında hazırlanmıştır.");
+        //}
+
+        public abstract class Ürün
+        {
+            public abstract void Siparis();
+            public List<string> Malzemeler { get; set; }
+            public double Fiyat { get; set; }
+            public string ÜrünAdı { get; set; }
+        }
+        public class IslakHamburger : Ürün
+        {
+            public override void Siparis()
+            {
+                Console.WriteLine($"{ÜrünAdı} IslakHamburger siparişiniz {Fiyat} TL tutarında hazırlanmıştır.");
+            }
+        }
+        public class Patso : Ürün
+        {         
+            public override void Siparis()
+            {
+                 Console.WriteLine($"{ÜrünAdı} Patso siparişiniz {Fiyat} TL tutarında hazırlanmıştır.");
+            }
+        }
+        public class Creater
+        {
+            public Ürün FactoryMethod(Ürünler ürünTipi)
+            {
+                Ürün ürün;
+
+                _ = ürünTipi switch
+                {
+                    Ürünler.Patso =>  ürün = new Patso(),
+                    Ürünler.IslakHamburger => ürün = new IslakHamburger(),
+                    _ => throw new ArgumentException("error")
+                };
+
+                return ürün;
+            }
+        }
+        public enum Ürünler
+        {
+            IslakHamburger,
+            Patso
+        }
     }
 }
--- 4.Design Patterns/Program.cs
+++ 4.Design Patterns/Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using static _4.Design_Patterns.Creational_Design_Pattern.FactoryMethodDesignPattern;
+
+//Console.WriteLine("Hello, World!");
+
+
+Creater creater = new Creater();
+
+Ürün ıslakHamburger = creater.FactoryMethod(Ürünler.IslakHamburger);
+Ürün patso = creater.FactoryMethod(Ürünler.Patso);
+
+ıslakHamburger.ÜrünAdı = "Meksika Acılı";
+ıslakHamburger.Fiyat = 80;
+
+ıslakHamburger.Siparis();
+
+patso.ÜrünAdı = "Amerikan Salatalı";
+patso.Fiyat = 70;
+
+patso.Siparis();
+
+//IslakHamburger ıslak = new IslakHamburger();
+//Patso patso =new Patso();
