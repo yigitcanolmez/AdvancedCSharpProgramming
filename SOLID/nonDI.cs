cc812aa184f678653d248260993de0b221a6b48f
yigitcanolmez <yigitcanolmez@outlook.com>
2023-10-11 07:12:28 +0300

    dependency inversion

---
+++ SOLID/nonDI.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace SOLID
+{
+    public interface IDatabaseService
+    {
+        void Connect();
+        void Disconnect();
+    }
+    public class Database : IDatabaseService
+    {
+        public void Connect()
+        {
+            Console.WriteLine("Database bağlan!");
+        }
+        public void Disconnect()
+        {
+            Console.WriteLine("Database bağlantısı kopar!");
+        }
+    }
+    public class User
+    {
+        private readonly IDatabaseService _database;
+        public User(IDatabaseService database)
+        {
+            _database = database;
+        }
+
+        public void Login()
+        {
+            _database.Connect();
+
+            //logic
+
+            _database.Disconnect();
+
+        }
+
+    }
+}
+
